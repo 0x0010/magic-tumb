d82387c36684916ea9aba372b6a587939f4df35b
Sam <sam.iagd@hotmail.com>
2017-03-13 15:14:51 +0800

    feat: receive xml request body

--- magic-tumb-wechat-intf/src/main/java/com/iamdigger/magictumblr/wcintf/controller/WeChatAccessController.java
+++ magic-tumb-wechat-intf/src/main/java/com/iamdigger/magictumblr/wcintf/controller/WeChatAccessController.java
@@ -2,9 +2,9 @@ package com.iamdigger.magictumblr.wcintf.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,9 +17,8 @@ public class WeChatAccessController {
   private static Logger logger = LoggerFactory.getLogger(WeChatAccessController.class);
 
   @RequestMapping(value = "/wechat/intf", method = RequestMethod.GET)
-  public String portalInFirstTime(@RequestParam String signature, @RequestParam String timestamp,
-      @RequestParam String nonce, @RequestParam String echostr) {
-    logger.info("signature:{}, timestamp:{}, nonce:{}, echostr:{}", signature, timestamp, nonce, echostr);
-    return echostr;
+  public String portalInFirstTime(@RequestBody String requestBody) {
+    logger.info("Request Body:{}", requestBody);
+    return "";
   }
 }
