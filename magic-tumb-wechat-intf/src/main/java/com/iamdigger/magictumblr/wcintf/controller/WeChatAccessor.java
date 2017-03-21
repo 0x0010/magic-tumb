6bfdb0f92e94163b16fbcd91ea6205c27d499dc9
Sam <sam.iagd@hotmail.com>
2017-03-21 10:06:19 +0800

    feat: 修改魔法溢出与艾露恩的祝福

--- magic-tumb-wechat-intf/src/main/java/com/iamdigger/magictumblr/wcintf/controller/WeChatAccessor.java
+++ magic-tumb-wechat-intf/src/main/java/com/iamdigger/magictumblr/wcintf/controller/WeChatAccessor.java
@@ -63,7 +63,7 @@ public class WeChatAccessor {
           break;
 
         case EVENT:
-          if("subscribe".equals(inTextMsg.getEvent())) {
+          if ("subscribe".equals(inTextMsg.getEvent())) {
             // 关注
             textReplyMsg.setContent(subscribeWelcome());
           } else {
@@ -134,79 +134,36 @@ public class WeChatAccessor {
         }
         return sb.toString();
       } else if (inText.equals("魔法书")) {
-       String magicBook = "";
+        String magicBook = "";
         magicBook += "❡初级魔法❡\n\n";
         magicBook += " ※ 月光林地中潜伏着被诅咒且扭曲的暗夜精灵撒特，她们驱赶着误食费伍德森林污水的熊怪来袭击这里的路人。这些熊怪有个可怕的「魔法溢出」技能实在是让人窒息。\n\n";
-        magicBook += " ※ 玛法里奥·怒风，这位最伟大的德鲁伊，用其强大的力量击败了试图入侵魔法森林的燃烧军团。一副魔法卷轴，被遗忘在了燃烧军团溃败的战场中，其中的奥义，就连强大的德莱尼都无法解读。"
-            + "最终玛法里奥将其永远留在了这个卡利姆多最安全的土地上，封印于魔法汤泉。因为魔法汤泉可以影射出这部卷轴的奥秘，那是一幅幅转瞬即逝的画面，让人过目难忘。玛法里奥只记得那部神秘卷轴的内容"
-            + "是以「TUMBLR」开始的。\n\n";
+        magicBook +=
+            " ※ 玛法里奥·怒风，这位最伟大的德鲁伊，用其强大的力量击败了试图入侵魔法森林的燃烧军团。一副魔法卷轴，被遗忘在了燃烧军团溃败的战场中，其中的奥义，就连强大的德莱尼都无法解读。"
+                + "最终玛法里奥将其永远留在了这个卡利姆多最安全的土地上，封印于魔法汤泉。因为魔法汤泉可以影射出这部卷轴的奥秘，那是一幅幅转瞬即逝的画面，让人过目难忘。玛法里奥只记得那部神秘卷轴的内容"
+                + "是以「TUMBLR」开始的。\n\n";
         return magicBook;
       } else if (inText.equals("魔法溢出")) {
         List<MagicAssetDO> top5Asset = magicAssetService.queryTop5Asset();
-        String magicBook = "被污染的熊怪经常使用该技能让经过这里的路人产生幻觉，这些幻觉由一些奇特的画面组成，让沉浸其中的人们不能自拔。\n";
-        magicBook += "魔法汤泉不经意间收集到了它们为非作歹的证据，以下是玛法里奥从圣泉中发现的一些片段：\n\n";
-        if(null != top5Asset && top5Asset.size() > 0) {
-          for(MagicAssetDO mad : top5Asset) {
-            magicBook += "⋉"+ mad.getVideoCode() +"⋊\n";
+        String magicBook = " ❡ 被污染的熊怪经常使用该技能让经过这里的路人产生幻觉，这些幻觉由一些奇特的画面组成，让沉浸其中的人们不能自拔。\n";
+        magicBook += "❡ 魔法汤泉不经意间收集到了它们为非作歹的证据，以下是玛法里奥从圣泉中发现的一些片段：\n\n";
+        if (null != top5Asset && top5Asset.size() > 0) {
+          for (MagicAssetDO mad : top5Asset) {
+            magicBook += "⋉" + mad.getVideoCode() + "⋊\n";
           }
         }
         magicBook += "\n";
-        magicBook += "玛法里奥也发现了解密这些证据的方法：删除首尾的魔法符号，并用中间部分拉丁字符替换神器钥石「https://vt.tumblr.com/tumblr_**.mp4」的星号，使用超世代的迅雷工具即可还原那些可恶熊怪的卑劣行径。";
+        magicBook += " ❡ 玛法里奥也发现了解密这些证据的方法，「艾露恩的祝福」或许会是解密这些证据的关键，我的朋友。";
         return magicBook;
+      } else if (inText.equals("艾露恩的祝福")) {
+        String aluen = " ❡ 月神湖浩淼庞大，它位于月光林地的中心。波光粼粼的水面就像是满天星光映衬下的月色，于是人们赋予了它这个充满传奇色彩的名字。也许湖水得到过「艾露恩的祝福」，从而成为最为纯净和健康的水源之一。\n\n";
+        aluen += " ❡ 删除首尾的魔法符号，并用中间部分拉丁字符替换神器钥石「https://vt.tumblr.com/tumblr_**.mp4」的星号，辅以超世代的迅雷获取可以还原那些熊怪的卑劣行径。\n\n";
+        return aluen;
       } else {
         // 非指令文本，直接写入文件，等待入库
         magicAssetFileService.saveToDisk(committer, inText);
         return i18nResource.getMessage("contentReceived1") + "\n" + i18nResource
             .getMessage("contentReceived2");
       }
-
-/*
-      OperationType ot = OperationType.of("");
-      String dispatchResult = "";
-      switch (ot) {
-        case G:
-          if (inText.length() <= 2) {
-            throw new MagicException(String.format(i18nResource.getException("e0001"), inText));
-          }
-          String assetCode = magicAssetFileService.saveToDisk(inText.substring(2, inText.length()));
-          dispatchResult = String.format(i18nResource.getMessage("urlReceived"), assetCode);
-          break;
-        case Q:
-          if (inText.length() <= 2) {
-            throw new MagicException(String.format(i18nResource.getException("e0001"), inText));
-          }
-          MagicAssetDO mad = magicAssetService.queryMagicAsset(inText.substring(2, inText.length()));
-          if(null != mad) {
-            AssetState assetState = AssetState.valueOf(mad.getState());
-            switch (assetState) {
-              case SUCCESS:
-                dispatchResult = String.format(i18nResource.getMessage("videoCodeFound"), mad.getVideoCode());
-                break;
-              case FAILED:
-                dispatchResult = i18nResource.getMessage("urlParseFailed");
-                break;
-              case PROCESSING:
-                dispatchResult = i18nResource.getMessage("underProcess");
-                break;
-              case UNSUPPORTED_URL:
-                dispatchResult = String.format(i18nResource.getMessage("unsupportedUrl"), mad.getOriginalUrl());
-                break;
-              case INIT:
-                dispatchResult = i18nResource.getMessage("waitForParse");
-                break;
-              default:
-                dispatchResult = i18nResource.getMessage("dizzy");
-            }
-          } else {
-            dispatchResult = i18nResource.getMessage("noAssetFound");
-          }
-          break;
-        case H:
-          break;
-        default:
-          break;
-      }
-      return dispatchResult;*/
     } catch (RuntimeException re) {
       logger.error("", re);
       if (re instanceof MagicException) {
